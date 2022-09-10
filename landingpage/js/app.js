a53ce046d9d3967585fd0b6c96564a7dd0bd1552
thinhtethtetzaw778 <thinhtethtetzaw778@gmail.com>
2022-09-10 14:00:28 +0630

    update

---
+++ landingpage/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // console.log('hay');
+
+    // START SHOWCASE SLIDER SECTION
+    $('.carousel').carousel({
+        interval:6000,
+        pause:'hover'
+    }) 
+    // END SHOWCASE SLIDER SECTION 
+});
